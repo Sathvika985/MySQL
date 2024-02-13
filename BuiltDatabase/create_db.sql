3e0888879c5cb459365b2bbc1ebc1c215f895387
sathvika <biryaniiii02@gmail.com>
2024-02-13 13:58:34 +0530

    create countries table,create cities table

--- BuiltDatabase/create_db.sql
+++ BuiltDatabase/create_db.sql
@@ -13,3 +13,35 @@ CREATE TABLE currencies(
     currency_name VARCHAR(100) NOT NULL,
     currency_code VARCHAR(3)
 );
+
+--2.create countries TABLE
+DROP TABLE IF EXISTS countries;
+CREATE TABLE countries(
+    country_id INT AUTO_INCREMENT PRIMARY KEY,
+    country_name VARCHAR(50) NOT NULL,
+    country_code VARCHAR(50),
+    region VARCHAR(50),
+    sub_region VARCHAR(100),
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    --create our foregin KEY
+    currency_id INT,
+    FOREIGN KEY (currency_id) REFERENCES currencies(currency_id)
+);
+
+--3.create cities TABLE
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities(
+    city_id INT AUTO_INCREMENT PRIMARY KEY,
+    city_name VARCHAR(50) NOT NULL,
+    latitude DECIMAL(8,4),
+    longitude DECIMAL(8,4),
+    capital VARCHAR(50),
+    population INT,
+    inserted TIMESTAMP DEFAULT CURRENT TIMESTAMP,
+
+    --create the foreign KEY
+    country_id INT,
+    FOREIGN KEY(country_id) REFERENCES countries(country_id)
+);
+
